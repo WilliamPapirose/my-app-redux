ff505f49cc9dc03475902517f7a6f54049719d33
WilliamPapirose <WilliamPapirose@github.com>
2019-01-27 18:47:05 +0600

    logOut header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Header = ({ user, signIn }) => (
+const Header = ({ user, logOut }) => (
   <div className="hat">
     <p className="hatUserName">{user}</p>
-    <button type="button" className="button hat_button" onClick={() => { signIn(''); }}>Exit profile</button>
+    <button type="button" className="button hat_button" onClick={logOut}>Exit profile</button>
   </div>
 );
 
 Header.propTypes = {
   user: PropTypes.string.isRequired,
-  signIn: PropTypes.func.isRequired,
+  logOut: PropTypes.func.isRequired,
 };
 
 export default Header;
--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -7,7 +7,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  signIn: () => {
+  logOut: () => {
     dispatch(signIn(''));
   },
 });
