702bd32478023399408263f40a104fd8cca00b10
WilliamPapirose <WilliamPapirose@github.com>
2019-01-27 19:06:32 +0600

    close by ESC

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,38 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ColumnContainer from '../containers/ColumnContainer';
 import SignInContainer from '../containers/SignInContainer';
 import HeaderContainer from '../containers/HeaderContainer';
 import CardInfoPopupContainer from '../containers/CardInfoPopupContainer';
 
+class Board extends Component {
+  componentDidMount = () => {
+    window.addEventListener('keydown', this.closePopupByEsc);
+  }
 
-const Board = ({ user, columns, cardInfoIsShowed }) => (
-  <React.Fragment>
-    { !user && (
-      <div role="presentation" className="fade">
-        <SignInContainer />
-      </div>
-    )}
-    { user && (
+  componentWillUnmount = () => {
+    window.removeEventListener('keydown', this.closePopupByEsc);
+  }
+
+  closePopupByEsc = (event) => {
+    const { hideCardInfoPopup } = this.props;
+    const keyValue = event.key;
+    if (keyValue === 'Escape') {
+      event.preventDefault();
+      hideCardInfoPopup();
+    }
+  }
+
+  render() {
+    const { user, columns, cardInfoIsShowed } = this.props;
+    return (
       <React.Fragment>
-        <HeaderContainer />
-        { cardInfoIsShowed && (
-          <CardInfoPopupContainer />
+        { !user && (
+          <div role="presentation" className="fade">
+            <SignInContainer />
+          </div>
         )}
-        <div className="all_columns">
-          {columns.map(column => (
-            <ColumnContainer key={column.id} id={column.id} columnName={column.columnName} />
-          ))}
-        </div>
+        { user && (
+          <React.Fragment>
+            <HeaderContainer />
+            { cardInfoIsShowed && (
+              <CardInfoPopupContainer />
+            )}
+            <div className="all_columns">
+              {columns.map(column => (
+                <ColumnContainer key={column.id} id={column.id} columnName={column.columnName} />
+              ))}
+            </div>
+          </React.Fragment>
+        )}
       </React.Fragment>
-    )}
-  </React.Fragment>
-);
+    );
+  }
+}
 
 Board.propTypes = {
   user: PropTypes.objectOf(PropTypes.string),
   columns: PropTypes.arrayOf(PropTypes.any).isRequired,
   cardInfoIsShowed: PropTypes.bool.isRequired,
+  hideCardInfoPopup: PropTypes.func.isRequired,
 };
 
 Board.defaultProps = {
--- src/containers/BoardContainer.js
+++ src/containers/BoardContainer.js
@@ -1,5 +1,7 @@
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import Board from '../components/Board';
+import { hideCardInfoPopup } from '../actions/index';
 
 const mapStateToProps = state => ({
   user: state.app.user,
@@ -8,8 +10,11 @@ const mapStateToProps = state => ({
   currentCard: state.app.current.card,
 });
 
+const mapDispatchToProps = dispatch => bindActionCreators({ hideCardInfoPopup }, dispatch);
+
 const BoardContainer = connect(
   mapStateToProps,
+  mapDispatchToProps,
 )(Board);
 
 export default BoardContainer;
